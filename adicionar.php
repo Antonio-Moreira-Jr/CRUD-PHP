1d0a18da1874a969811e7c81755461a0b1ea6ae8
smario88 <silviomario@outlook.com>
2022-03-31 16:50:37 -0300

    Ultima Atualização 31/03/2022
    Adicionando Input de Arquivos em Cadastro de Cliente.
    
    Co-Authored-By: Antonio-Moreira-Jr <61807682+Antonio-Moreira-Jr@users.noreply.github.com>

--- adicionar.php
+++ adicionar.php
@@ -6,7 +6,7 @@ include_once 'includes/header.php';
 ?>
 <section class="conteudo">
 <div class= "row">
-      <div class= "col s12 m6 push-m3 ">
+      <div class= "col s12 m6 push-m3">
         <h3 class="light">Cadastro de Cliente</h3>
             <form action="php_action/create.php" method="POST">
                 <div class="input-field col s12">
@@ -21,10 +21,21 @@ include_once 'includes/header.php';
                     <input type="email" name="email" id="email" required>
                     <label for="email">Email</label>
                 </div>
-                <div class="input-field col s12">
+                <div class="input-field col s2">
                     <input type="number" name="idade" id="idade" required>
                     <label for="idade">Idade</label>
                 </div>
+                 <!-- INICIO Imput Imagem -->
+                    <div class="file-field input-field col s10">                    
+                        <div class="btn blue darken-2">                        
+                            <input type="file" multiple>
+                            <span>Imagem do Cliente</span>                            
+                        </div>                        
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text" placeholder="Upload one or more files">                            
+                        </div>
+                    </div>
+                <!-- FIM Imput Imagem -->                
                 <div class="center">
                 <button type="submit" name="btn-cadastrar" class="btn">Cadastrar Cliente</button>
                 <a href="main.php" class="btn blue darken-2">Lista de Clientes</a>
